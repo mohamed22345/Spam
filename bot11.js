4a57b8d1c1ab91e4bc959666dff00d257b419625
mohamed22345 <44568787+mohamed22345@users.noreply.github.com>
2018-10-29 12:36:18 +0300

    Update bot11.js

--- bot11.js
+++ bot11.js
@@ -27,7 +27,7 @@ client.on('ready', async() => {
 var server = "506394514685034496"; // ايدي السررفر
 var channel = "506394514685034498";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('#rep <@314135031029170197>')
+    client.guilds.get(server).channels.get(channel).send('#rep <@431535348867268608>')
     },8.64e+7);
 })
 
