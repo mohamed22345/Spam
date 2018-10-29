e25dcb2b4abf9ad7db8ce590daa66b8699d90fb9
mohamed22345 <44568787+mohamed22345@users.noreply.github.com>
2018-10-29 12:37:02 +0300

    Update bot13.js

--- bot13.js
+++ bot13.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "506084858762756106"; // ايدي السررفر
-var channel = "506106229001551872";//ايدي الروم
+var server = "506394514685034496"; // ايدي السررفر
+var channel = "506394514685034498";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('** فرعونفؤعونفؤعونفؤعونفؤعونفؤعونفرعونفؤعونفؤعونفؤعونفؤعونفرعونفرعونفرعونفؤعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفؤعونفؤعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفرعونفؤعونفرعونفرعونفؤعونفرعونفرعونفؤعونفؤعونفؤعونفؤعونفؤعونفؤعونفرعونفرعونفرعونفرعونفرعونفؤعونفرعونفرعونفرعومفرعون **')
     },305);
@@ -16,25 +16,25 @@ var channel = "506106229001551872";//ايدي الروم
 
 
 client.on('ready', async() => {
-var server = "506084858762756106"; // ايدي السررفر
-var channel = "506106229001551872";//ايدي الروم
+var server = "506394514685034496"; // ايدي السررفر
+var channel = "506394514685034498";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('#daily')
     },8.64e+7);
 })
 
 client.on('ready', async() => {
-var server = "506084858762756106"; // ايدي السررفر
-var channel = "506106229001551872";//ايدي الروم
+var server = "506394514685034496"; // ايدي السررفر
+var channel = "506394514685034498";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('#rep <@314135031029170197>')
+    client.guilds.get(server).channels.get(channel).send('#rep <@431535348867268608>')
     },8.64e+7);
 })
 
 
 client.on('message', message => {
 if(message.content.startsWith('13s')) {
-if(message.author.id !== "420637382644072451") return;
+if(message.author.id !== "431535348867268608") return;
 var args = message.content.split(' ').slice(1).join(' ');
 message.channel.send(args);
 }
